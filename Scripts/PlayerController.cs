c884a9194b40b3a2e1bb50e46d690b1d55fdb39f
DrHorn <bhorn@trinity.edu>
2019-09-05 13:26:16 -0500

    Initial commit

---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed = 5f;
+	public float jumpSpeed = 5f;
+	private float movement = 0f;
+	private Rigidbody2D rigidBody2D;
+	public Transform groundCheckPoint;
+	public float groundCheckRadius;
+    public LayerMask groundLayer;
+	private bool isOnGround;
+	public Vector3 respawnPoint;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+		respawnPoint = transform.position;
+    }
+
+	// Update is called once per frame
+	void Update()
+	{
+		isOnGround = Physics2D.OverlapCircle(groundCheckPoint.position,groundCheckRadius,groundLayer);
+		movement = Input.GetAxis("Horizontal");
+		if (movement != 0f)
+		{
+			rigidBody2D.velocity = new Vector2(movement * speed, rigidBody2D.velocity.y);
+		}
+		else
+		{
+			rigidBody2D.velocity = new Vector2(0, rigidBody2D.velocity.y);
+		}
+
+		if (Input.GetButtonDown("Jump") && isOnGround)
+        {
+			rigidBody2D.velocity = new Vector2(rigidBody2D.velocity.x, jumpSpeed);
+		}
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+	{
+        if ("FallDetector" == other.tag)
+		{
+			Debug.Log("Fall Detected");
+			gameObject.SetActive(false);
+			gameObject.transform.position = respawnPoint;
+			gameObject.SetActive(true);
+		}
+	}
+}
---
+++ Scripts/TriangleCollectionScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriangleCollectionScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Collided");
+        Destroy(gameObject);
+        ScoreScript.scoreValue += 10;
+    }
+}
